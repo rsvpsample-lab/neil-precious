7810d6bec9f5a4337dfd1d3b065fb45009aa961a
rsvpsample-lab <rsvpsample@gmail.com>
2025-11-08 10:04:59 +0000

    new logo

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Menu, X } from 'lucide-react';
 
+import Monogram_removebg_preview_DaokIrKn from "@assets/Monogram-removebg-preview-DaokIrKn.png";
+
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [activeSection, setActiveSection] = useState('');
@@ -91,7 +93,7 @@ const Navigation = () => {
           {/* Logo */}
           <div className="flex-shrink-0">
             <img 
-              src="https://res.cloudinary.com/dr3xey7h9/image/upload/v1760170580/17ea81a6-dbcd-4dbc-b598-ab591dc176c2-removebg-preview_vfbeaq.png" 
+              src={Monogram_removebg_preview_DaokIrKn} 
               alt="P&D Logo" 
               className="h-12 w-auto"
             />
@@ -129,7 +131,6 @@ const Navigation = () => {
           </div>
         </div>
       </div>
-
       {/* Mobile Navigation */}
       {isOpen && (
         <div className="md:hidden">
